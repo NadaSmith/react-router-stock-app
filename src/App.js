2d0b1e996f125d8fb08f974339fb2211f2fb70db
Nada Smith <ayronadas@gmail.com>
2023-07-25 14:47:04 -0400

    Removing outdated Switch component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import './App.css';
 import React from 'react';
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Stock from "./pages/Stock";
 import About from "./pages/About"
 import stocks from './data';
-import Dasbhoard from './components/Dashboard';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <Router className="App">
       <Navbar />
-      <Switch>
+      <Routes>
         <Route exact path="/" component={Home} />
         <Route path="/about" componenet={About} />
         <Route exact path="/stocks" component={Dashboard} />
         <Route path="/stocks: symbol" component={Stock} />
-      </Switch>
+      </Routes>
     </Router>
   );
 }
 
 export default App;
+
+//Router provides the routing functionality 
+//Switch ensures that only one route is rendered at a time;
+//Route defines mapping btw URLs and the components that should be rendered when those URLs are matched
--- src/pages/Stock.js
+++ src/pages/Stock.js
@@ -1,6 +1,7 @@
 import React from "react";
+import stocks from "../data";
 
-export default function Stock (props) {
+export default function Stock () {
     
     /* props.stock object
     const stock = (props) => {
@@ -20,13 +21,13 @@ export default function Stock (props) {
 
     return (
         <div>
-            <h2>{name}</h2>
-            <p>Symbol: {symbol} </p>
-            <p>Last Price: {lastPrice} </p>
-            <p>Change: {change} </p>
-            <p>High: {high} </p>
-            <p>Low: {low} </p>
-            <p>Open: {open} </p>
+            <h2>{stocks.name}</h2>
+            <p>Symbol: {stocks.symbol}</p>
+            <p>Last Price: {stocks.lastPrice}</p>
+            <p>Change: {stocks.change}</p>
+            <p>High: {stocks.high}</p>
+            <p>Low: {stocks.low}</p>
+            <p>Open: {stocks.open}</p>
         </div>
     )
 }
